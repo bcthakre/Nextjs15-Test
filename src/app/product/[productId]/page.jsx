7202bfca2b35d0e305d6d5ae9c10800fdc82b84a
bcthakre <bcthakregcp@gmail.com>
2024-01-05 13:17:26 -0600

    Add NotFound component and handle 404 errors in product and review pages

--- src/app/product/[productId]/page.jsx
+++ src/app/product/[productId]/page.jsx
@@ -1,6 +1,12 @@
+import { notFound } from "next/navigation"
+
 export default function ProductDetail({ params }) {
 
     console.log(params)
+
+    if (parseInt(params.productId) > 20) {
+        notFound()
+    }
     return (
         <div>
             <h1>Product Details {params.productId}</h1>
--- src/app/product/[productId]/reviews/[reviewId]/page.jsx
+++ src/app/product/[productId]/reviews/[reviewId]/page.jsx
@@ -1,6 +1,13 @@
+import { notFound } from "next/navigation"
+
+
 export default function ProductReview({ params }) {
 
     console.log(params)
+
+    if ((parseInt(params.productId) >20 || parseInt(params.reviewId) > 100)) {
+        notFound()
+    }
     return (
         <div>
             <h1>Review with {params.reviewId} about Product Name {params.productId}</h1>
