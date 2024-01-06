3f79950744211cf4a5b91101534c60a6aa843cdf
bcthakre <bcthakregcp@gmail.com>
2024-01-05 19:04:16 -0600

    Add PrivateFolder component and update NotFound component

---
+++ src/app/_lib/page.jsx
@@ -0,0 +1,7 @@
+export default function PrivateFolder() {
+    return (
+        <div>
+            <h1>Private Folder</h1>
+        </div>
+    )
+}
--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -2,6 +2,7 @@ export default function NotFound() {
     return (
         <div>
             <h1>Not Found Page</h1>
+            <h2> This page does not exists</h2>
         </div>
     )
 }
