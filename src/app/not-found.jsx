c4132682a31337817ea19d85e2c0f227a15d89b2
bcthakre <bcthakregcp@gmail.com>
2024-01-06 18:36:57 -0600

    Add login, register, and forgot password pages

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -3,6 +3,7 @@ export default function NotFound() {
         <div>
             <h1>Not Found Page</h1>
             <h2> This page does not exists</h2>
+            <h3> This has been created by BT</h3>
         </div>
     )
 }
