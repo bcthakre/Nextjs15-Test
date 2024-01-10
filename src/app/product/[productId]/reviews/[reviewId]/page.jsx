898013c3c6d0e7c061881f5d310eaab1ef14905d
bcthakre <bcthakregcp@gmail.com>
2024-01-09 22:00:41 -0600

    Delete about page and add error handling to product review

--- src/app/product/[productId]/reviews/[reviewId]/page.jsx
+++ src/app/product/[productId]/reviews/[reviewId]/page.jsx
@@ -1,9 +1,21 @@
 import { notFound } from "next/navigation"
 
+function getRandomInt(count) {
+    return Math.floor(Math.random() * count)
+}
+
 
 export default function ProductReview({ params }) {
 
     console.log(params)
+
+    const random = getRandomInt(2)
+
+    console.log(random)
+
+    if (random === 1){
+        throw new Error('Error created by me')
+    }
 
     if ((parseInt(params.productId) >20 || parseInt(params.reviewId) > 100)) {
         notFound()
