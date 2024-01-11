47c64b210168df1c7569e5c9b19bb3381d37f6ed
bcthakre <bcthakregcp@gmail.com>
2024-01-11 11:36:02 -0600

    Fix bug in login functionality

---
+++ src/app/cd/@users/default.jsx
@@ -0,0 +1,7 @@
+import Card from "@/components/card";
+
+export default function DefaultUsers() {       
+    return (
+       <Card>User List for Default.jsx</Card>
+    )
+}
---
+++ src/app/cd/default.jsx
@@ -0,0 +1,7 @@
+export default function DefaultCommplexDashboard() {
+    return (
+        <div>
+            <h1> Commplex Dashboard  for Default.jsx</h1>
+        </div>
+    )
+}
