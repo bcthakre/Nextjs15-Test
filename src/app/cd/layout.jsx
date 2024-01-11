823d2d14e0931c9cd9e66ffc2ffc211ebb7abcae
bcthakre <bcthakregcp@gmail.com>
2024-01-10 23:17:09 -0600

    Add new pages and layout components

--- src/app/cd/layout.jsx
+++ src/app/cd/layout.jsx
@@ -1,7 +1,25 @@
-export default function RootLayout({ children }) {
+// export default function RootLayout({ children }) {
+//   return (
+//     <html lang="en">
+//      <div>{ children }</div>
+//      <UserAnalytics />
+//      <RevenueMetrics />
+//      <NotificationMetrics />
+//     </html>
+//   );
+// }
+
+export default function Layout({ children, revenue, users, notifications }) {
   return (
     <html lang="en">
-     <div>{ children }</div>
+      <div>{children}</div>
+      <div style={{ display: "flex" }}>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <div>{users}</div>
+          <div>{revenue}</div>
+        </div>
+        <div style={{ display: "flex", flex: 1 }}>{notifications}</div>
+      </div>
     </html>
   );
 }
