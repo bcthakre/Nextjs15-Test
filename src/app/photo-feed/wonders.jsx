2e907ba448825af7986dd8996eeaedf3f8551de2
bcthakre <bcthakregcp@gmail.com>
2024-01-12 19:59:30 -0600

    starting parallel and intercepting route

---
+++ src/app/photo-feed/wonders.jsx
@@ -0,0 +1,30 @@
+import photo1 from "./photos/1.jpg";
+import photo2 from "./photos/2.jpg";
+import photo3 from "./photos/3.jpg";
+
+
+const wondersImages = [
+  {
+    id: "1",
+    name: "Great Wall of China",
+    src: photo1,
+    photographer: "Photo by Max van den Oetelaar on Unsplash",
+    location: "China",
+  },
+  {
+    id: "2",
+    name: "Petra",
+    src: photo2,
+    photographer: "Photo by Reiseuhu on Unsplash",
+    location: "Jordan",
+  },
+  {
+    id: "3",
+    name: "Christ the Redeemer",
+    src: photo3,
+    photographer: "Photo by Andrea Leopardi on Unsplash",
+    location: "Brazil",
+  },
+];
+
+export default wondersImages;
